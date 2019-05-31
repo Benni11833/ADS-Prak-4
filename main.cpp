24679f99c647e37ac4cdb0828501fe09347689f7
Benni11833 <benedikt.happe@alumni.fh-aachen.de>
2019-05-31 11:41:37 +0200

    Prak4Up

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include "sorting.h"
+
+#define NumbersInLine 8
+
+void Print_Vec(std::vector<int>& arr){
+    std::cout << std::endl << std::endl;
+    for(int i=0; i < arr.size(); i++){
+        if( i != 0 && i%NumbersInLine == 0)
+            std::cout << std::endl;
+        std::cout << std::setw(10) << std::left << arr[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+}
+
+struct Timer{
+    //std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::high_resolution_clock::time_point start, end;
+    std::chrono::duration<float> duration;
+
+   Timer(){
+         start = std::chrono::high_resolution_clock::now();
+   }
+   void reset(){
+       start = std::chrono::high_resolution_clock::now();
+   }
+   auto getDuration(){
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        return duration.count();
+   }
+   void printDuration(){
+       end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        std::cout << "Timer lief " << duration.count() << " Sekunden" << std::endl;
+   }
+   /*~Timer(){
+        end = std::chrono::high_resolution_clock::now();
+        duration = end - start;
+        std::cout << "Timer lief " << duration.count() << " Sekunden" << std::endl;
+   }*/
+};
+
+int main(int argc, char* argv[]){
+    long unsigned int N{0L};
+    if(argc == 1){
+        std::cout << "Anz der Elemente: ";
+        std::cin >> N;
+    }else{
+        N = atol(argv[1]);
+    }
+    std::cout << "Starte Sortieren mit " << N << " Elementen.\n";
+    Timer t1;
+    std::vector<int> rnd_vec;
+    std::vector<int> sort_vec = rnd_vec;
+    sort_vec.resize(N);
+    rnd_vec.resize(N);
+    sorting::randomizeVector(rnd_vec, N);
+    sort_vec = rnd_vec;
+    t1.reset();
+    sorting::QuickSort(sort_vec, 0, sort_vec.size());
+    std::cout << "Quicksort lief " << t1.getDuration() << " Sekunden\n";
+    
+    sort_vec = rnd_vec;
+    /*sort_vec.resize(5);
+    sort_vec[0] = 1; sort_vec[1] = 7; 
+    sort_vec[2] = 3; sort_vec[3] = 5;
+    sort_vec[4] = 2;*/
+    //Print_Vec(sort_vec);
+    t1.reset();
+    vector<int> x;
+    sorting::MergeSort(sort_vec, x, 0, sort_vec.size());
+    std::cout << "Mergesort lief " << t1.getDuration() << " Sekunden\n";
+    /*std::cout << std::endl << "MergeSorted:\n" << std::endl << std::endl;
+    Print_Vec(sort_vec);*/
+    return 0;
+}
+
+/*TODO
+Shellsort (Hibbard Folge)
+HeapSort
+->Messzeiten in TextDatei anzeigen
+ */
---
+++ sorting.cpp
@@ -0,0 +1,146 @@
+#include "sorting.h"
+
+
+namespace sorting {
+
+	//************
+	// QuickSort *
+	//************      
+	void QuickSort(vector<int> &arr, int left, int right) {
+
+		if(left < right){
+			int mid = partition(arr, left, right);
+			QuickSort(arr, left, mid-1);
+			QuickSort(arr, mid+1, right);
+		}
+
+	}
+
+void swap(int &val1, int &val2){
+	int tmp{val1};	val1 = val2;	val2 = tmp;
+}
+
+int partition(std::vector<int> &arr, int left, int right){
+		int x = arr[right];
+		int i = left-1;
+		for(int j=left; j <= right-1; j++){
+			if(arr[j] <= x){
+				++i;
+				swap(arr[i], arr[j]);
+			}
+		}
+		swap(arr[i+1], arr[right]);
+		return i+1;
+	}
+
+	//************
+	// MergeSort *
+	//************
+
+	void Merge(vector<int> &a, vector<int> &b, int low, int pivot, int high) {
+		int n1{pivot-low};
+		if(high == a.size())
+			--high;
+		b.clear();	b.resize(high-low+1);
+		for(int i=0; i < b.size(); i++){
+			b[i] = a[low+i];
+		}
+		
+		/*std::cout << "TestAusgabe:\n\n";
+		std::cout << "a[low-high]:\n";
+		for(int i=low; i < high; i++)
+			std::cout << a[i] << ",";
+		std::cout << std::endl << "tmp[0-(high-low):\n";
+		for(int i=0; i < b.size(); i++)
+			std::cout << b[i] << ",";
+		std::cout << std::endl << std::endl;
+		std::cout << "sollen gleich sein: " << a[pivot] << " - " << b[pivot-low] << std::endl;
+		std::cin.get();*/
+
+		/*std::cout << "low=" << low << " - piv=" << pivot << " - high="
+		<< high  << " - n1=" << n1 << std::endl;
+		std::cout << std::endl << "tmp[0-(high-low):\n";
+		for(int i=0; i < b.size(); i++)
+			std::cout << b[i] << ",";
+		std::cout << std::endl;*/
+
+		//einsortieren aus tmp in a
+		int i{0}, j{n1+1}, k{low};
+		for(k=low; k < high; k++){
+			if(b[i] <= b[j] && i <= n1){
+				a[k] = b[i++];
+			}
+			else if(j <= high-low){
+				a[k] = b[j++];
+			}
+			else
+				break;
+		}
+		//reste in TeilArrays in a einsortieren
+		while(i <= n1 && k <= high)
+			a[k++] = b[i++];
+		while(j < high-low && k < high)
+			a[k++] = b[j++];
+		
+		/*std::cout << "TestAusgabe:\n\n";
+		std::cout << "a[low-high]:\n";
+		for(int i=low; i <= high; i++)
+			std::cout << a[i] << ",";
+		std::cout << std::endl << "a[ges]:\n";
+		for(int i=0; i < a.size(); i++)
+			std::cout << a[i] << ",";
+		std::cin.get();*/
+	}
+
+	void MergeSort(vector<int> &a, vector<int> &b, int low, int high) {
+		if(low < high){
+			int q = (low+high)/2;
+			MergeSort(a, b, low, q);
+			MergeSort(a, b, q+1, high);
+			Merge(a, b, low, q, high);
+		}
+	}
+
+	//************
+	// Heapsort  *
+	//************
+
+	void HeapSort(vector<int> &a, int n) {
+
+
+		//***************************
+		// implement heapsort here *
+		//***************************
+
+	}
+
+
+
+	//************
+	// Shellsort *
+	//************
+	void ShellSort(vector<int> &a, int n)
+	{
+
+		//***************************
+		// implement shellsort here *
+		//***************************
+
+
+	}
+
+
+  void randomizeVector(vector<int> &array, int n) {
+    array.resize(n);
+
+    for(unsigned int i=0;i<array.size();i++)
+      array[i]=rand() % 1000000;
+  }
+
+
+}
+
+
+
+
+
